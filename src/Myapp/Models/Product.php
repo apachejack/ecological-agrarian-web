afa8a86d8a4632d809d25c827ad9b31a874cbf40
José Carlos Gómez <heypixelweb@gmail.com>
2015-03-26 00:45:15 +0100

    Products now have custom pics from the uploads dir

--- src/Myapp/Models/Product.php
+++ src/Myapp/Models/Product.php
@@ -2,6 +2,8 @@
 namespace Myapp\Models;
 use Silex\Application;
 
+/* Las imágenes que muestren al principio los productos deben ser siempre las más recientes */
+
 class Product{
 	protected $app;
 	protected $idProduct;
@@ -12,16 +14,38 @@ class Product{
 	}
 
 	public function getDetails(){
-		$query = "SELECT * 
+		$query = "SELECT p.title, 
+						p.description, 
+						p.created_at, 
+						p.id, 
+						s.status, 
+						i.path as image_path 
 				FROM productos as p 
+				LEFT JOIN estados_cosecha as s 
+				ON (p.idstatus = s.id) 
+				LEFT JOIN images as i 
+				ON (p.id = i.idProduct)
 				WHERE p.id = :idProduct 
+				ORDER BY i.id DESC 
 				LIMIT 1";
 
 		$stmt = $this->app["db"]->prepare($query);
 		$stmt->bindValue(":idProduct", $this->idProduct, "integer");
 		$stmt->execute();
 
-		return $stmt->fetch();
+		$product = $stmt->fetch();
+		$product["image_path"] = Product::formatImagePath($product["image_path"]);
+
+
+		return $product;
+
+	}
+
+	public static function formatImagePath($path){
+		if(is_null($path)){
+			return $path;
+		}
+		return "uploads/images/".$path;
 	}
 
 }
--- src/Myapp/Models/ProductsSearch.php
+++ src/Myapp/Models/ProductsSearch.php
@@ -1,6 +1,9 @@
 <?php
 namespace Myapp\Models;
 use Silex\Application;
+use Myapp\Models\Product;
+
+/* Las imágenes que muestren los productos deben ser siempre las más recientes */
 
 class ProductsSearch{
 	protected $app;
@@ -10,14 +13,36 @@ class ProductsSearch{
 	}
 
 	public function getProducts(){
-		$query = "SELECT * 
+		$query = "SELECT p.title, 
+						p.description, 
+						p.id, 
+						s.status, 
+						p.updated_at, 
+						(SELECT images.path 
+						FROM images 
+						WHERE images.idProduct = p.id 
+						ORDER BY images.id DESC LIMIT 1) as image_path  
 				FROM productos as p 
+				LEFT JOIN estados_cosecha as s 
+				ON (p.idstatus = s.id) 
+				GROUP BY p.id 
 				ORDER BY p.updated_at DESC";
 
 		$stmt = $this->app["db"]->prepare($query);
 		$stmt->execute();
 
-		return $stmt->fetchAll();
+		$products = $stmt->fetchAll();
+		$products = $this->formatAllImagesPaths($products);
+
+		return $products;
+	}
+
+	protected function formatAllImagesPaths(array $products){
+		array_walk($products, function(&$product){
+			$product["image_path"] = Product::formatImagePath($product["image_path"]);
+		});
+
+		return $products;
 	}
 
 	public function setSearch(array $data){
